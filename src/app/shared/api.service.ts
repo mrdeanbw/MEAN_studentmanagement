d36ef8de6c946c12890715403245d17f24858af6
SinghDigamber <digambersingh126@gmail.com>
2019-06-15 00:55:37 +0530

    commit with love

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -11,7 +11,7 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 export class ApiService {
 
   // endpoint: string = 'http://localhost:4000/api';
-  endpoint: string = 'api';
+  endpoint: string = '/api';
   headers = new HttpHeaders().set('Content-Type', 'application/json');
 
   constructor(private http: HttpClient) { }
